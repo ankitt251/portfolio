ce4f0578df05384c30cd805274d08e497983c286
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-02 11:00:11 +0530

    spa fall back changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust this value as needed
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })

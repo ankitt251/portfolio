41ae0f680c1e1afff538b7c6f9f78e965174ab6e
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-02 11:15:36 +0530

    revert

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import React from "react";
-import {
-  About,
-  Contact,
-  Experience,
-  Feedbacks,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-} from "./components";
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
-        <Switch>
-          <Route exact path="/" component={About} />
-          <Route path="/experience" component={Experience} />
-          <Route path="/tech" component={Tech} />
-          <Route path="/works" component={Works} />
-          <Route path="/feedbacks" component={Feedbacks} />
-        </Switch>
-        <div className="relative z-0">
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;

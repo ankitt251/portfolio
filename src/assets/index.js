c8534cad6d7c1edf5953a255868e74089ea22009
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-03 22:43:34 +0530

    added icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import letter from "./letter-a.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -39,6 +40,7 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
+  letter,
   backend,
   creator,
   mobile,
